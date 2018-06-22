05ddf51777eee8613eb66014c254614575b805be
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-06-22 16:02:56 -0700

    Reorders folder structure.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,37 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import Clock from './components/Clock.jsx';
-import './styles/App.css';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
-
-  componentDidMount() {
-  }
-
-  render () {
-    return (
-      <div className="app">
-        <header>
-          <div className="title">
-            <h1 className="title__text">Motivational Alarm</h1>
-          </div>
-          <div className="auth-buttons">
-            <button className="sign-up">Sign Up</button>
-            <button className="log-in">Log In</button>
-          </div>
-        </header>
-        <div className="clock"><Clock /></div>
-        <div className="alarm-list">Alarm List</div>
-        <div className="form">Form</div>
-    </div>
-    )
-  }
-}
+import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
